9a14601dba10762a02eb6104cf6dfecd43834644
Boitumelo Chombo <boitumelo.chombo@studentmail.biust.ac.bw>
2016-10-11 13:19:31 +0200

    Update label_creator.sh

--- digits/extensions/data/objectDetection/label_creator.sh
+++ digits/extensions/data/objectDetection/label_creator.sh
@@ -1,9 +1,10 @@
-#!/bin/bash 
-for F in ./path to folder containing your images/*.jpeg; do
+for F in ./path to image containing folder/*.jpeg; do
    	T=${F##*/}  ## Removes directory part. Same as $(basename "$F")
-	identify -format "%w %h" ./path to folder containing your image/$T 
-   	T=${T%.jpeg}.txt  ## Removes .JPEG and adds .txt
-	identify -format "%wx%h" $T
-echo "type  0 0 0 0 0 0 0 0 0 0 0 0 0 0 " >> $T
+	D=$(identify -format "%w " ./path to image containing folder/$T) 
+	E=$(identify -format "%h" ./path to folder containing images/$T)
+   	N=${T%.jpeg}.txt  ## Removes .JPEG and adds .txt
+	
+echo "type 0 0 0 0 $D  0 $E 0 0 0 0 0 0 0 " >> ./label/$N # write the data to an already existing label folder
+
    
-done
+done 
